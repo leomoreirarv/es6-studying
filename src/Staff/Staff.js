f5368b0d04f2478ecb819944746cb9bf22bf7e50
Leonardo Moreira <leonardo.moreira@fleetmatics.com>
2017-12-18 17:00:04 +0000

    extending constructor

--- src/Staff/Staff.js
+++ src/Staff/Staff.js
@@ -1,5 +1,16 @@
 import {Person} from '../Person/Person.js';
 
 export class Staff extends Person{
+    constructor(name, gender, birthdate, sector){
+        super(name, gender, birthdate);
+        this._sector = sector;
+    }
 
+    get sector(){
+        return this._sector;
+    }
+
+    set sector(value){
+        this._sector = value;
+    }
 }
--- src/Student/Student.js
+++ src/Student/Student.js
@@ -1,5 +1,16 @@
 import {Person} from '../Person/Person.js';
 
 export class Student extends Person{
-   
+   constructor(name, gender, birthdate, reg){
+       super(name, gender, birthdate);
+        this._reg = reg;
+   }
+
+   get reg(){
+       return this._reg;
+   }
+
+   set reg(value){
+       this._reg = reg;
+   }
 }
